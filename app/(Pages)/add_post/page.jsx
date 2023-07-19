18f7b1dff6a105c96abbee85f0e5e96da02bb79b
Ameer Alhasnawi <46317379+Ameerusa86@users.noreply.github.com>
2023-07-18 23:16:09 -0400

    -

--- app/(Pages)/add_post/page.jsx
+++ app/(Pages)/add_post/page.jsx
@@ -1,12 +1,10 @@
 import PostForm from "../../../Components/PostForm";
-// import UploadDnD from "@/app/api/upload-dnd/page";
 import React from "react";
 
 const AddPost = () => {
   return (
     <div>
       <PostForm />
-      {/* <UploadDnD /> */}
     </div>
   );
 };
